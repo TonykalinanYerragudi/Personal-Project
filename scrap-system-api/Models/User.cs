bb620ca5bb6d3956b19b517fff4cb5892b4c3299
Tony Kalinan <yerraguditonykalinan@gmail.com>
2025-07-27 09:33:51 +0530

    usermodel

---
+++ scrap-system-api/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace scrap_system_api.Models;
+
+public class User
+{
+  public int UserId { get; set; }
+  public string? FullName { get; set; }
+  public string? Email { get; set; }
+  public string? PhoneNumber { get; set; }
+  public string? Role { get; set; }
+  public DateTime CreatedAt { get; set; }
+
+}
